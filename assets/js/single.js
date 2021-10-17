4ad5d1ea964548bf8f74b6cb094d93bda30c1023
arios10 <ariosfly3@gmail.com>
2021-10-17 01:08:04 -0700

    linking pages and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,27 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    //call getRepoIssues() using parameter
+    getRepoIssues(repoName);
+  }
+  else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -16,8 +35,8 @@ var getRepoIssues = function(repo) {
             });
           }
         else {
-            console.log(response);
-            alert("There was a problem with your request!");
+            // if repo is invalid, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -69,4 +88,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
   
-  getRepoIssues("facebook/react");
+getRepoName();
